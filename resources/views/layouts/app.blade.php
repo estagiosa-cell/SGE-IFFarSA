2ae9750697b69df501a9954e92cf83c2a5c83100
willers <arthur2008willers@gmail.com>
2025-06-21 11:48:44 -0300

    adicionar favicon SVG do Instituto Federal ao projeto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.svg') }}">
   <title>@yield('title') - SIGE-IFFarSA</title>
   @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
