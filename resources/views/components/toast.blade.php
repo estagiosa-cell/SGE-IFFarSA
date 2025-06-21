2f16991c1c54bd84df3b07a5ba7f9096e06becb6
willers <arthur2008willers@gmail.com>
2025-06-21 13:52:34 -0300

    adicionar componente Toast e integrá-lo ao layout principal

---
+++ resources/views/components/toast.blade.php
@@ -0,0 +1,20 @@
+@if (session('message'))
+  @php
+    // Define a classe do toast com base no tipo de mensagem
+    // Se não houver tipo definido, usa 'warning' como padrão
+    $toastClass = 'text-bg-' . session('messageType', 'warning');
+  @endphp
+  {{-- Toast do Bootstrap para exibir mensagens --}}
+  <div class="toast-container position-fixed top-0 start-50 translate-middle-x p-3">
+    <div id="toastMessage" class="toast align-items-center {{ $toastClass }} border-0" role="alert"
+      aria-live="assertive" aria-atomic="true">
+      <div class="d-flex">
+        <div class="toast-body">
+          {{ session('message') }}
+        </div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
+          aria-label="Close"></button>
+      </div>
+    </div>
+  </div>
+@endif
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,26 +10,7 @@
 </head>
 
 <body>
-
-  @if (session('message'))
-    @php
-      $toastClass = 'text-bg-' . session('messageType', 'warning');
-    @endphp
-
-    {{-- Toast do Bootstrap para exibir mensagens --}}
-    <div class="toast-container position-fixed top-0 start-50 translate-middle-x p-3">
-      <div id="toastMessage" class="toast align-items-center {{ $toastClass }} border-0" role="alert"
-        aria-live="assertive" aria-atomic="true">
-        <div class="d-flex">
-          <div class="toast-body">
-            {{ session('message') }}
-          </div>
-          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast"
-            aria-label="Close"></button>
-        </div>
-      </div>
-    </div>
-  @endif
+  <x-toast />
 
   @yield('content')
 
