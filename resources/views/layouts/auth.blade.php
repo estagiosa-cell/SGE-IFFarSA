e99ef9e39a477488a162ea0faeade4120d89286e
willers <arthur2008willers@gmail.com>
2025-07-11 19:37:04 -0300

    feat: implementa grid responsivo no layout auth
    Sidebar 2 colunas + conteúdo 10 colunas

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  <x-sidebar />
-
-  @yield('main-content')
+<div class="container-fluid">
+  <div class="row">
+      <x-sidebar />
+    <div class="col-lg-10">
+      @yield('main-content')
+    </div>
+  </div>
+</div>
 @endsection
