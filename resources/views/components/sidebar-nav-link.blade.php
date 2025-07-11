e546c74553f71a419913b56f06916ff01d7df1b8
willers <arthur2008willers@gmail.com>
2025-07-11 18:13:05 -0300

    feat: adiciona componente sidebar-nav-link para links de navegação

---
+++ resources/views/components/sidebar-nav-link.blade.php
@@ -0,0 +1,19 @@
+@props([
+    'icon' => '',
+    'active' => false,
+    'route' => null,
+])
+
+@php
+  $isActive = $active || ($route && request()->routeIs($route));
+  $url = $route ? route($route) : '#';
+@endphp
+
+<li class="nav-item mb-2">
+  <a class="nav-link {{ $isActive ? 'active' : 'text-dark' }}" href="{{ $url }}">
+    @if ($icon)
+      <i class="bi bi-{{ $icon }} me-2"></i>
+    @endif
+    {{ $slot }}
+  </a>
+</li>
