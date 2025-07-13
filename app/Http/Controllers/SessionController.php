61f62472f3278fdc691a88a1cae32b4ea1dc036b
willers <arthur2008willers@gmail.com>
2025-07-13 13:39:45 -0300

    feat: implementa LoginRequest para validação do login

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -5,42 +5,53 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Gate;
 
 class SessionController extends Controller
 {
+    /**
+     * Exibe o formulário de login
+     */
     public function create()
     {
         return view('auth.login');
     }
 
-    public function store(Request $request)
+    /**
+     * Processa o login do usuário
+     */
+    public function store(LoginRequest $request)
     {
-        $attributes = $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
 
-        if (! Auth::attempt($attributes)) {
+        // Tenta autenticar o usuário com as credenciais fornecidas
+        if (! Auth::attempt($request->validated())) {
+            // Se falhar, lança uma exceção de validação com mensagem de erro
             throw ValidationException::withMessages([
                 'login_error' => __('auth.failed'),
             ]);
         }
 
-        $request->session()->regenerate();
+        // Se a autenticação for bem-sucedida
+        session()->regenerate();
 
+        // Redireciona o usuário para a rota pretendida ou para o dashboard
         return redirect()->intended(route('dashboard'))
             ->with('message', 'Usuário autenticado com sucesso!')
             ->with('messageType', 'success');
     }
 
-    public function destroy(Request $request)
+    /**
+     * Realiza o logout do usuário
+     */
+    public function destroy()
     {
         Auth::logout();
 
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+        session()->invalidate();
+        session()->regenerateToken();
 
-        return redirect(route('login'))
+        return redirect()->route('login')
             ->with('message', 'Você foi deslogado com sucesso!')
             ->with('messageType', 'success');
     }
