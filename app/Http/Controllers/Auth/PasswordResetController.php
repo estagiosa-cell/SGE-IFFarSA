b9fe4105fd352d6eaab619719d4cb73d04c3164a
willers <arthur2008willers@gmail.com>
2025-07-17 13:53:07 -0300

    feat: implementa método create no PasswordResetController

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class PasswordResetController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('auth.passwords.email');
+    }
 }
