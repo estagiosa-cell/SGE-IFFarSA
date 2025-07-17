5365e775af57ce07aa1cf6a9855eed7e353a52fa
willers <arthur2008willers@gmail.com>
2025-07-17 15:03:59 -0300

    feat: implementa método store no PasswordResetController para envio de link de redefinição de senha

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class PasswordResetController extends Controller
 {
     public function create()
     {
         return view('auth.passwords.email');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::ResetLinkSent
+            ? back()->with(['status' => __($status)])
+            : back()->withErrors(['email' => __($status)]);
     }
 }
