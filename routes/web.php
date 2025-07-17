0a987eb862d6c00599b91663f02fd6452c4feb3a
willers <arthur2008willers@gmail.com>
2025-07-17 13:55:10 -0300

    fix: corrige método HTTP da rota de reset de senha para

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::post('/login', [SessionController::class, 'store']);
 Route::get('/forgot-password', [PasswordResetController::class, 'create'])->name('password.request');
 Route::post('/forgot-password', [PasswordResetController::class, 'store'])->name('password.email');
 Route::get('/reset-password/{token}', [PasswordResetController::class, 'edit'])->name('password.reset');
-Route::post('/reset-password', [PasswordResetController::class, 'update'])->name('password.update');
+Route::put('/reset-password', [PasswordResetController::class, 'update'])->name('password.update');
 
 
 // Rotas dos usuários autenticados
