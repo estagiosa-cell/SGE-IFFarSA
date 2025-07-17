ee6271295d6320ddb7f10ec4012d6eea9a67d3f3
willers <arthur2008willers@gmail.com>
2025-07-17 15:46:33 -0300

    feat: implementa método update no PasswordResetController para redefinição de senha

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 
 class PasswordResetController extends Controller
 {
@@ -29,5 +33,31 @@ class PasswordResetController extends Controller
     public function edit($token)
     {
         return view('auth.passwords.reset', ['token' => $token]);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ]);
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PasswordReset
+            ? redirect()->route('login')->with('message', __($status))->with('messageType', 'success')
+            : back()->withErrors(['email' => [__($status)]]);
     }
 }
