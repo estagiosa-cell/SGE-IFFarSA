74d857a102433c0f737db4e75615333ccacac7b5
willers <arthur2008willers@gmail.com>
2025-07-17 13:33:20 -0300

    refactor: muda diretório do SessionController.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\SessionController;
+use App\Http\Controllers\Auth\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [SessionController::class, 'create'])->name('login');
