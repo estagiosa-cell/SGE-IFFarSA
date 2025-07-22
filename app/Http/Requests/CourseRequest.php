318aab146370199f760a2822dc119a6b718cd363
willers <arthur2008willers@gmail.com>
2025-07-22 08:40:09 -0300

    Renomeia arquivo

--- app/Http/Requests/CourseRequest.php
+++ app/Http/Requests/CourseRequest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Validation\Rule;
 
-class StoreCourseRequest extends FormRequest
+class CourseRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
