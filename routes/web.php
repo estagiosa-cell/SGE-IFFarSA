71b91e4e6fb073e9909bdd12c59de3229e4dea16
willers <arthur2008willers@gmail.com>
2025-07-28 10:41:52 -0300

    feat: adiciona rota para deletar usuário

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
         Route::put('/users/{id}', [UserController::class, 'update'])->name('admin.users.update');
         Route::post('/users/import', [UserController::class, 'import'])->name('admin.users.import');
+        Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
         Route::patch('/users/{user}/deactivate', [UserController::class, 'deactivate'])->name('admin.users.deactivate');
         Route::patch('/users/{user}/reactivate', [UserController::class, 'reactivate'])->name('admin.users.reactivate');
 
