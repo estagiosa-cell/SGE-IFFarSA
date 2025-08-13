e68bb2bc442bcf3c922da17a2e3483bb82f3e32f
willers <arthur2008willers@gmail.com>
2025-08-13 14:55:45 -0300

    feat: add link for managing accreditations in sidebar navigation

--- resources/views/components/sidebar-nav.blade.php
+++ resources/views/components/sidebar-nav.blade.php
@@ -24,6 +24,10 @@
             Gerenciar Cursos
         </x-sidebar-nav-link>
 
+        <x-sidebar-nav-link route="admin.accreditations.index" icon="person-badge">
+            Gerenciar Credenciamentos
+        </x-sidebar-nav-link>
+
         <x-sidebar-nav-link route="admin.internship-types.index" icon="tags">
             Gerenciar Tipos de Estágio
         </x-sidebar-nav-link>
