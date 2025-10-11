af529b4d941b64b50a4fc8397728ae7265cd7c2d
ArthurWillers <arthurwillers@proton.me>
2025-10-11 11:22:29 -0300

    refactor: consolidate user table migrations by adding deactivated_at and softDeletes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('role');
+            $table->timestamp('deactivated_at')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
 
