2cf7e1cb661c3404801f7ad62baa631a6dc460bc
ArthurWillers <arthurwillers@proton.me>
2025-10-29 18:46:55 -0300

    feat: Update backup instructions for clarity and detail

--- resources/views/admin/backup.blade.php
+++ resources/views/admin/backup.blade.php
@@ -18,12 +18,22 @@
                     gerado estará no formato <strong>.zip</strong> e conterá todos os dados necessários para restaurar o
                     sistema. Além disso, o arquivo será salvo nos arquivos do software.
                 </p>
-                <p><strong>Instruções:</strong></p>
+                <p><strong>Instruções para Backup:</strong></p>
                 <ul>
-                    <li>Clique no botão "Fazer Backup" abaixo.</li>
-                    <li>Aguarde o download do arquivo <code>backup-sge-ANO-MES-DIA_HORA-MINUTO-SEGUNDO.zip</code>.</li>
-                    <li>Armazene o arquivo em um local seguro para futuras restaurações.</li>
+                    <li>Clique no botão "Fazer Backup Completo" abaixo.</li>
+                    <li>Aguarde o download do arquivo <code>NOME-DA-APP-ANO-MES-DIA_HORA-MINUTO-SEGUNDO.zip</code>.</li>
+                    <li>Armazene o arquivo em um local seguro.</li>
                 </ul>
+
+                <hr>
+
+                <p><strong>Instruções para Restauração:</strong></p>
+                <ol>
+                    <li><strong>Extraia o arquivo .zip</strong> que você baixou.</li>
+                    <li>Dentro da pasta extraída, você encontrará uma subpasta chamada <code>db-dumps</code>. Dentro dela, haverá um arquivo com final <code>.sql</code>. <strong>Este é o seu backup do banco de dados.</strong></li>
+                    <li>Em um ambiente novo, execute o comando <code>php artisan migrate</code> para criar a estrutura inicial do banco de dados.</li>
+                    <li>Importe o arquivo <code>.sql</code> para o seu banco de dados.</li>
+                </ol>
 
                 <form method="POST" action="{{ route('admin.backup.create') }}"
                     class="d-flex justify-content-end no-spinner">
