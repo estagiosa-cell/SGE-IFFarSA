8ae94db633c34cc13ebfe6b51eed87945d894a20
ArthurWillers <arthurwillers@proton.me>
2025-11-05 18:51:36 -0300

    chore: remove outdated task scheduling for password reset token cleanup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->withSchedule(function (Schedule $schedule): void {
-        // Limpa os tokens de recuperação de senha expirados diariamente
-        $schedule->command('auth:clear-resets')->daily();
+        //
     })
     ->create();
