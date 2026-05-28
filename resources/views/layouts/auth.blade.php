72ea43303ef07ec9652e56248ca355289cf7a212
Arthur Willers <arthurwillers@proton.me>
2026-05-28 13:14:44 -0300

    feat: move header markup to the top for better structure and clarity

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,26 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row m-0">
+    {{-- Header para telas pequenas --}}
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary d-lg-none shadow-sm sticky-top">
+        <div class="container-fluid">
+            <button class="navbar-toggler border-0 p-2" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#sidebarOffcanvas">
+                <i class="bi bi-layout-sidebar-inset fs-5"></i>
+            </button>
+            <span class="navbar-brand fw-bold fs-4 position-absolute start-50 translate-middle-x">
+                SGE-IFFarSA
+            </span>
+        </div>
+    </nav>
+
+    <div class="container-fluid px-0 px-md-3">
+        <div class="row g-0">
             <div class="col-lg-2 p-0">
                 <x-sidebar />
             </div>
 
             <main class="col-lg-10 p-0 pb-3">
-                {{-- Header para telas pequenas --}}
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary d-lg-none shadow-sm sticky-top">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler border-0 p-2" type="button" data-bs-toggle="offcanvas"
-                            data-bs-target="#sidebarOffcanvas">
-                            <i class="bi bi-layout-sidebar-inset fs-5"></i>
-                        </button>
-                        <span class="navbar-brand fw-bold fs-4 position-absolute start-50 translate-middle-x">
-                            SGE-IFFarSA
-                        </span>
-                    </div>
-                </nav>
-
                 @yield('main-content')
             </main>
         </div>
