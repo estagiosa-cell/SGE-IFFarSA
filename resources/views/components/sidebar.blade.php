8e3da9d18029724b9da9e1f2067bd681f2e1ef4f
Arthur Willers <arthurwillers@proton.me>
2026-06-02 11:42:00 -0300

    feat: add system logo to sidebar and offcanvas headers

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,6 +1,9 @@
 {{-- Sidebar para telas grandes --}}
 <div class="d-none d-lg-flex bg-white flex-column vh-100 p-2 border-end position-sticky top-0">
-    <h5 class="m-2">SGE-IFFarSA</h5>
+    <h5 class="m-2 d-flex align-items-center">
+        <img src="{{ Vite::asset('resources/images/favicon.svg') }}" alt="Logo IFFarSA" class="me-2" style="height: 24px; width: auto;">
+        SGE-IFFarSA
+    </h5>
     <hr>
     <div class="flex-grow-1 overflow-y-auto overflow-x-hidden">
         <x-sidebar-nav />
@@ -24,7 +27,10 @@
 {{-- Offcanvas para telas pequenas --}}
 <div class="offcanvas offcanvas-start" tabindex="-1" id="sidebarOffcanvas">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="sidebarOffcanvasLabel">SGE-IFFarSA</h5>
+        <h5 class="offcanvas-title d-flex align-items-center" id="sidebarOffcanvasLabel">
+            <img src="{{ Vite::asset('resources/images/favicon.svg') }}" alt="Logo IFFarSA" class="me-2" style="height: 24px; width: auto;">
+            SGE-IFFarSA
+        </h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
     <div class="offcanvas-body d-flex flex-column">
